9479b77bc7cf816fe31c72c10a953d4e0755fc67
Alexander Kuznetsov <aleksander.kuznetsov@jetbrains.com>
2024-08-16 12:22:05 +0200

    Adjust to new changes in coroutines

--- src/main/java/com/intellij/rt/debugger/agent/CaptureAgent.java
+++ src/main/java/com/intellij/rt/debugger/agent/CaptureAgent.java
@@ -600,13 +600,16 @@ public final class CaptureAgent {
         // Flows
 
         // SharedFlow
-        addCapture("kotlinx/coroutines/flow/SharedFlowImpl$FlowValueWrapper", CONSTRUCTOR, THIS_KEY_PROVIDER);
+        addCapture("kotlinx/coroutines/flow/internal/FlowValueWrapperInternal", CONSTRUCTOR, THIS_KEY_PROVIDER);
         addInsert("kotlinx/coroutines/flow/SharedFlowImpl", "emitInner", param(1));
 
         // StateFlow
         addCapture("kotlinx/coroutines/flow/StateFlowImpl", "updateInner", FIRST_PARAM);
         addCapture("kotlinx/coroutines/flow/StateFlowImpl", CONSTRUCTOR, FIRST_PARAM);
         addInsert("kotlinx/coroutines/flow/StateFlowImpl", "emitInner", param(1));
+
+        // debounce
+        addInsert("kotlinx/coroutines/flow/FlowKt__DelayKt", "emitInner$FlowKt__DelayKt", param(1));
       }
     }
   }
--- src/main/java/com/intellij/rt/debugger/agent/SharedFlowTransformer.java
+++ src/main/java/com/intellij/rt/debugger/agent/SharedFlowTransformer.java
@@ -5,58 +5,56 @@ import org.jetbrains.capture.org.objectweb.asm.*;
 import java.lang.instrument.ClassFileTransformer;
 import java.security.ProtectionDomain;
 
+import static org.jetbrains.capture.org.objectweb.asm.Opcodes.F_SAME;
+
 class SharedFlowTransformer implements ClassFileTransformer {
     @Override
     public byte[] transform(final ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) {
-        if (!"kotlinx/coroutines/flow/SharedFlowImpl".equals(className)) {
+        if (!"kotlinx/coroutines/flow/internal/FlowValueWrapperInternalKt".equals(className)) {
             return classfileBuffer;
         }
         ClassReader reader = new ClassReader(classfileBuffer);
-        ClassWriter writer = new ClassWriter(reader, ClassWriter.COMPUTE_FRAMES) {
-            @Override
-            protected ClassLoader getClassLoader() {
-                return loader;
-            }
-        };
+        ClassWriter writer = new ClassWriter(reader, 0);
         reader.accept(new ClassVisitor(Opcodes.API_VERSION, writer) {
             @Override
             public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
                 MethodVisitor mv = super.visitMethod(access, name, descriptor, signature, exceptions);
                 switch (name) {
-                    case "wrap":
+                    case "wrapInternal":
                         return new WrapMethodTransformer(mv);
-                    case "unwrap":
+                    case "unwrapInternal":
                         return new UnwrapMethodTransformer(mv);
                 }
                 return mv;
             }
         }, 0);
-        return writer.toByteArray();
+        byte[] bytes = writer.toByteArray();
+        CaptureAgent.storeClassForDebug(className, bytes);
+        return bytes;
     }
 
     /*
-        Default wrap is:
+        Default wrapInternal is:
         ```
-          private fun <T> wrap(value: T): T = value
+          internal fun <T> wrapInternal(value: T): T = value
         ```
 
         which compiles to bytecode
         ```
-          aload_1
+          aload_0
           areturn
         ```
 
         The transformed code should call the function
         ```
-          private fun wrapDebuggerCapture(value: Any?) = FlowValueWrapper(value)
+          private fun wrapInternalDebuggerCapture(value: Any?): Any = FlowValueWrapperInternal(value)
         ```
 
         so the transformation should yield the bytecode
         ```
            0: aload_0
-           1: aload_1
-           2: invokespecial #530                // Method wrapDebuggerCapture:(Ljava/lang/Object;)Ljava/lang/Object;
-           5: areturn
+           1: invokestatic  #10                 // Method wrapInternalDebuggerCapture:(Ljava/lang/Object;)Ljava/lang/Object;
+           4: areturn
         ```
      */
     private static class WrapMethodTransformer extends MethodVisitor {
@@ -69,35 +67,36 @@ class SharedFlowTransformer implements ClassFileTransformer {
             super.visitCode();
 
             mv.visitVarInsn(Opcodes.ALOAD, 0);
-            mv.visitVarInsn(Opcodes.ALOAD, 1);
-            mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "kotlinx/coroutines/flow/SharedFlowImpl", "wrapDebuggerCapture", "(Ljava/lang/Object;)Ljava/lang/Object;", false);
+            mv.visitMethodInsn(Opcodes.INVOKESTATIC, "kotlinx/coroutines/flow/internal/FlowValueWrapperInternalKt", "wrapInternalDebuggerCapture", "(Ljava/lang/Object;)Ljava/lang/Object;", false);
             mv.visitInsn(Opcodes.ARETURN);
+            mv.visitFrame(F_SAME, 0, null, 0, null);
+            mv.visitMaxs(1, 1);
+            mv.visitEnd();
         }
     }
 
     /*
         Default unwrap is:
         ```
-          private fun <T> unwrap(value: T): T = value
+          internal fun <T> unwrapInternal(value: T): T = value
         ```
 
         which compiles to bytecode
         ```
-          aload_1
+          aload_0
           areturn
         ```
 
         The transformed code should call the function
         ```
-          private fun unwrapDebuggerCapture(value: Any?): Any? = (value as? FlowValueWrapper<*>)?.value ?: value
+          private fun unwrapInternalDebuggerCapture(value: Any?): Any? = (value as? FlowValueWrapperInternal<*>)?.value ?: value
         ```
 
         so the transformation should yield the bytecode
         ```
            0: aload_0
-           1: aload_1
-           2: invokespecial #530                // Method unwrapDebuggerCapture:(Ljava/lang/Object;)Ljava/lang/Object;
-           5: areturn
+           1: invokestatic  #16                 // Method unwrapInternalDebuggerCapture:(Ljava/lang/Object;)Ljava/lang/Object;
+           4: areturn
         ```
      */
     private static class UnwrapMethodTransformer extends MethodVisitor {
@@ -110,9 +109,11 @@ class SharedFlowTransformer implements ClassFileTransformer {
             super.visitCode();
 
             mv.visitVarInsn(Opcodes.ALOAD, 0);
-            mv.visitVarInsn(Opcodes.ALOAD, 1);
-            mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "kotlinx/coroutines/flow/SharedFlowImpl", "unwrapDebuggerCapture", "(Ljava/lang/Object;)Ljava/lang/Object;", false);
+            mv.visitMethodInsn(Opcodes.INVOKESTATIC, "kotlinx/coroutines/flow/internal/FlowValueWrapperInternalKt", "unwrapInternalDebuggerCapture", "(Ljava/lang/Object;)Ljava/lang/Object;", false);
             mv.visitInsn(Opcodes.ARETURN);
+            mv.visitFrame(F_SAME, 0, null, 0, null);
+            mv.visitMaxs(1, 1);
+            mv.visitEnd();
         }
     }
 }
