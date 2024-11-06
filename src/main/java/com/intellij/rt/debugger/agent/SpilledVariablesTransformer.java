9d825c248cc4f80277d9d6b933a5b36b6a571576
Maria Sokolova <maria.sokolova@jetbrains.com>
2024-11-06 13:57:52 +0100

    Transform nullOutSpilledVariable to see the value of the spilled object

---
+++ src/main/java/com/intellij/rt/debugger/agent/SpilledVariablesTransformer.java
@@ -0,0 +1,62 @@
+package com.intellij.rt.debugger.agent;
+
+import org.jetbrains.capture.org.objectweb.asm.*;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+
+import static org.jetbrains.capture.org.objectweb.asm.Opcodes.F_SAME;
+
+public class SpilledVariablesTransformer {
+    public static void init(Instrumentation instrumentation) {
+        if (Boolean.getBoolean("debugger.agent.enable.coroutines")) {
+            instrumentation.addTransformer(new SpillingTransformer(), true);
+        }
+    }
+
+    private static class SpillingTransformer implements ClassFileTransformer {
+        @Override
+        public byte[] transform(final ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) {
+            if (!"kotlin/coroutines/jvm/internal/SpillingKt".equals(className)) {
+                return classfileBuffer;
+            }
+            try {
+                ClassReader reader = new ClassReader(classfileBuffer);
+                ClassWriter writer = new ClassWriter(reader, 0);
+                reader.accept(new ClassVisitor(Opcodes.API_VERSION, writer) {
+                    @Override
+                    public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+                        MethodVisitor mv = super.visitMethod(access, name, descriptor, signature, exceptions);
+                        if ("nullOutSpilledVariable".equals(name)) return new NullOutSpilledVariableTransformer(mv);
+                        return mv;
+                    }
+                }, 0);
+                byte[] bytes = writer.toByteArray();
+                CaptureAgent.storeClassForDebug(className, bytes);
+                return bytes;
+            } catch (Exception e) {
+                System.out.println("SpillingTransformer: failed to instrument " + className);
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        private static class NullOutSpilledVariableTransformer extends MethodVisitor {
+            public NullOutSpilledVariableTransformer(MethodVisitor mv) {
+                super(Opcodes.API_VERSION, mv);
+            }
+
+            @Override
+            public void visitCode() {
+                super.visitCode();
+
+                mv.visitVarInsn(Opcodes.ALOAD, 0);
+                mv.visitInsn(Opcodes.ARETURN);
+                mv.visitFrame(F_SAME, 0, null, 0, null);
+                mv.visitMaxs(1, 1);
+                mv.visitEnd();
+            }
+        }
+    }
+}
