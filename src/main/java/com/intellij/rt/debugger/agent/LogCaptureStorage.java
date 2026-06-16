9b4301d789cba0d09adc8f8add33ce3ef01600e5
Maksim Zuev <maksim.zuev@jetbrains.com>
2026-06-16 16:13:47 +0200

    IDEA-390527 Add a flag to confirm flushing after evaluation
    - evaluation may fail due to object collected exception
    - with the introduced flag, confirmed data can be removed safely

--- src/main/java/com/intellij/rt/debugger/agent/LogCaptureStorage.java
+++ src/main/java/com/intellij/rt/debugger/agent/LogCaptureStorage.java
@@ -36,6 +36,7 @@ public class LogCaptureStorage {
 
     // It's used by the debugger.
     static final AtomicLong EVENT_COUNTER = new AtomicLong();
+    static volatile long CONFIRMED_FLUSHED_EVENT_ID = -1;
 
     // It contains raw events that are waiting to be packed.
     // New ones could be added concurrently.
@@ -83,6 +84,19 @@ public class LogCaptureStorage {
         @Override
         public boolean markRemoved() {
             return removed.compareAndSet(false, true);
+        }
+
+        @Override
+        public final boolean equals(Object o) {
+            if (!(o instanceof Event)) return false;
+
+            Event event = (Event) o;
+            return id == event.id;
+        }
+
+        @Override
+        public int hashCode() {
+            return (int) (id ^ (id >>> 32));
         }
     }
 
@@ -235,6 +249,7 @@ public class LogCaptureStorage {
      * before it appears on the debugger side. The clearing happens in the periodic flush cycle.
      */
     static String packBatchedData() throws IOException {
+        clearConfirmedBatches();
         packRawEvents();
         List<PackedBatch> packedBatchesSnapshot = new ArrayList<>(PACKED_BATCHES);
         if (packedBatchesSnapshot.isEmpty()) return null;
@@ -272,6 +287,7 @@ public class LogCaptureStorage {
     }
 
     private static void flushBatchedData(boolean forceOutput) throws IOException {
+        clearConfirmedBatches();
         if (forceOutput || currentEventsEstimatedBytes() > BUFFER_SIZE) {
             packRawEvents();
         }
@@ -280,9 +296,25 @@ public class LogCaptureStorage {
         if (packedBatchesSnapshot.isEmpty()) return;
 
         outputWritten(packBatches(packedBatchesSnapshot));
-        long removedBytes = removeItems(PACKED_BATCHES, packedBatchesSnapshot);
+        markBatchesFlushed(packedBatchesSnapshot);
+    }
+
+    private static void markBatchesFlushed(Collection<PackedBatch> batches) {
+        long removedBytes = removeItems(PACKED_BATCHES, batches);
         PACKED_BATCHES_BYTES.addAndGet(-removedBytes);
-        setIfGreater(LAST_FLUSHED_EVENT_ID, findMaxPackedEventId(packedBatchesSnapshot));
+        setIfGreater(LAST_FLUSHED_EVENT_ID, findMaxPackedEventId(batches));
+    }
+
+    private static void clearConfirmedBatches() {
+        long confirmedId = CONFIRMED_FLUSHED_EVENT_ID;
+        if (confirmedId <= LAST_FLUSHED_EVENT_ID.get()) return;
+        Set<PackedBatch> alreadyConfirmed = new HashSet<>();
+        for (PackedBatch batch : PACKED_BATCHES) {
+            if (batch.lastEventId <= confirmedId) {
+                alreadyConfirmed.add(batch);
+            }
+        }
+        markBatchesFlushed(alreadyConfirmed);
     }
 
     private static byte[] packBytes(List<Event> events) throws IOException {
@@ -388,7 +420,7 @@ public class LogCaptureStorage {
     }
 
     private static <T extends MemoryFootprintEstimate> long removeItems(ConcurrentLinkedQueue<T> queue, Collection<T> items) {
-        Set<T> itemsToRemove = new HashSet<>(items);
+        Set<T> itemsToRemove = items instanceof Set ? (Set<T>) items : new HashSet<>(items);
 
         long removedBytes = 0;
         for (Iterator<T> queueIterator = queue.iterator(); queueIterator.hasNext(); ) {
